c5ed93490fea8839e5d436e1ad7943dd1d2001d4
Ryan Tong <RyanyeeTong@gmail.com>
2025-06-05 15:41:40 -0500

    Initial Commit

---
+++ PromptMg/PromptMgApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptMgApp.swift
+//  PromptMg
+//
+//  Created by Ryan Tong on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PromptMgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptMgTests/PromptMgTests.swift
@@ -0,0 +1,17 @@
+//
+//  PromptMgTests.swift
+//  PromptMgTests
+//
+//  Created by Ryan Tong on 6/5/25.
+//
+
+import Testing
+@testable import PromptMg
+
+struct PromptMgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
